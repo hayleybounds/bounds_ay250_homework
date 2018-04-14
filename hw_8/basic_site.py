2af52fa778812c1e1cb431c6ef7b0996dea2241a
hayleybounds <hayleybounds@gmail.com>
2018-04-14 17:52:39 -0400

    Added collection reports to homepage, error handling, and more testing

--- hw_8/basic_site.py
+++ hw_8/basic_site.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template, request, url_for, redirect, flash
 import flask
 import pybtex.database as pb
 from flask_sqlalchemy import SQLAlchemy
+from config import Config
+import numpy as np
+from sqlalchemy.exc import OperationalError
 
 app = Flask(__name__)
-app.debug = True
-app.secret_key = 'Super Secret PW'
+app.config.from_object(Config)
 db=SQLAlchemy(app)
 
 #TODO: somehow move at least some of this to another file?
@@ -15,26 +17,31 @@ class Citation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     ReferenceTag = db.Column(db.String)
     Collection = db.Column(db.String)
-    #Adsnote = db.Column(db.String)
-    #Adsurl = db.Column(db.String)
     Author = db.Column(db.String)
-    #Date_Added = db.Column(db.Date)
-    #Date_Modified = db.Column(db.Date)
     Journal = db.Column(db.String)
     Keywords = db.Column(db.String)
-    #Month = db.Column(db.String)
-    Pages = db.Column(db.String) #I think?
+    Pages = db.Column(db.String)
     Title = db.Column(db.String)
     Volume = db.Column(db.Integer)
     Year = db.Column(db.Integer)
-    #Bdsk_File_1  = db.Column(db.String)
     
 db.create_all()
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('base.html', page_title="Home", message="Welcome! </p> this is my site")
+    """Welcomes the user and tells them how many collections are in the database."""
+    if Citation.query.first() is None: #check for empty db
+        this_message = 'You have no collections. Upload a file to get started!'
+    else: #get number of collections and names
+        collections = np.unique(Citation.query.with_entities(Citation.Collection).all())
+        colls_w_sizes = [s+' (%s entries)' %get_size_collection(s) for s in collections]
+        this_message = ('You have %s collections, with names : ' % len(collections)) + \
+                        ', '.join([s for s in colls_w_sizes]) 
+    
+    return render_template('base.html', page_title="Home",
+                           message="Welcome! </p>This is a site for querying bibtex files </p>"+
+                                   this_message)
 
 @app.route('/query_database/', methods=['GET', 'POST'])
 def query_db():
@@ -42,9 +49,13 @@ def query_db():
         query = request.form['query']
         #empty form submission doesn't return None, so don't check for that
         if query not in (""," "):
-            result=db.engine.execute(query).fetchall()
+            try:
+                result=db.engine.execute(query).fetchall()
+            except OperationalError:
+                return render_template('form.html', page_title= 'Query Database', 
+                                   message='Invalid query. Please try again.')
             if len(result) > 0:
-                display_query_results(x)
+                display_query_results(result)
             else:
                 flash('No results to display :(')
             return render_template('form.html', page_title= 'Results for : ' + query)
@@ -55,27 +66,22 @@ def query_db():
         ## this is a normal GET request
         return render_template("form.html", page_title = 'Query a Database :)')
     
-    
-    
 @app.route('/upload_file/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
             return render_template('file_up.html', message = 'Please Choose a File')
         file = request.files['file']
-        if file not in (""," "):
-            coll_name = request.form['collection_name']
-            if coll_name not in (""," ",None):
-                #read and convert to this database thing.
-                x=pb.parse_bytes(file.read(), bib_format='bibtex')
-                for k, entry in x.entries.items():
-                    add_entry_to_db(k, entry, coll_name)
-                return render_template('base.html', page_title= coll_name)
-            else:
-                return render_template('file_up.html',
-                                       message = 'Please enter a valid collection name')
+        coll_name = request.form['collection_name']
+        if coll_name not in (""," "):
+            #read and convert to this database thing.
+            x=pb.parse_bytes(file.read(), bib_format='bibtex')
+            for k, entry in x.entries.items():
+                add_entry_to_db(k, entry, coll_name)
+            return render_template('base.html', page_title= coll_name)
         else:
-            return render_template('file_up.html', message = 'Please Choose a File')
+            return render_template('file_up.html',
+                                   message = 'Please enter a valid collection name')
     else:
         ## this is a normal GET request
         return render_template("file_up.html", page_title = 'Upload a File :)')
@@ -85,20 +91,36 @@ def display_query_results(results):
               'Pages', 'Volume', 'Year']
     for row in results:
         for field in fields:
-            print(field)
-            flash(field + ': ' + str(row[field]))
-        flash('</br>')
-    
+            if field == 'Year':
+                flash(field + ': ' + str(row[field]) + '</br></br>')
+            else:
+                flash(field + ': ' + str(row[field]))
     
 def add_entry_to_db(key, entry, collection):
     keywords = {}
-    for field in ['Title', 'Author','Journal', 'Keywords', 'Pages', 'Volume', 'Year']:
+    for field in ['Title', 'Journal', 'Keywords', 'Pages', 'Volume', 'Year']:
         if field in entry.fields:
-            keywords[field] = entry.fields[field]
+            keywords[field] = format_btex_fields(entry.fields[field])
         else:
             keywords[field] = None
+    #handle author separately because its stored differently
+    if 'Author' in entry.persons:
+        author_list = [format_btex_fields(str(auth)) for auth in entry.persons['Author']]
+        keywords['Author'] = ', '.join(author_list)
+    else:
+        keywords['Author'] = None
     db.session.add(Citation(**keywords, Collection = collection, ReferenceTag = key))
     db.session.commit()
+    
+def format_btex_fields(string):
+    """helper method to remove odd formatting from btex files"""
+    string = string.replace('{','')
+    string = string.replace('}','')
+    return string
+
+def get_size_collection(collection):
+    """helper method to get the number of entries in a collection"""
+    return len(Citation.query.filter(Citation.Collection == collection).all())
     
 if __name__ == "__main__":
     app.run()
--- hw_8/testing_test.py
+++ hw_8/testing_test.py
@@ -1,8 +1,9 @@
 import os
 import unittest
 from basic_site import app
+from io import BytesIO
 
-class FlaskrTestCase(unittest.TestCase):
+class FlaskTestCase(unittest.TestCase):
 
     def setUp(self):
         #self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
@@ -26,12 +27,53 @@ class FlaskrTestCase(unittest.TestCase):
             query=query
         ), follow_redirects=True)
     
+    def test_query_page(self):
+        rv = self.app.get('/query_database/')
+        assert b'name="query"' in rv.data
+    
+    def test_upload_file_page(self):
+        rv = self.app.get('/upload_file/')
+        
+        assert b'Choose file to upload' in rv.data
+        assert b'Upload a File' in rv.data
+    
+    def upload_file(self, coll_name, filename, filecontent=b'SOME CONTENT'):
+        """helper method to mimic the user choosing a file and collection name"""
+        data = {
+            'collection_name': coll_name,
+            'file': (BytesIO(filecontent), filename)
+        }
+
+        return self.app.post('/upload_file/', buffered=True,
+                         content_type='multipart/form-data',
+                         data=data)
+        
+    def test_file_upload(self):
+        rv = self.upload_file('ex', 'hw_8_data/homework_8_refs.bib')
+        assert rv.status_code == 200
+        assert b'ex' in rv.data
+    
     def test_query_submission(self):
         """tests invalid queries and queries to empty databases"""
         assert b'Results for' in self.submit_query('SELECT * from bibliography').data
-        assert b'No results' in self.submit_query('SELECT * from bibliography').data
+        assert b'z' in self.submit_query('SELECT * from bibliography').data
         assert b'Invalid' in self.submit_query('').data
-        assert b'Invalid' in self.submit_query(' ').data        
+        assert b'Invalid' in self.submit_query(' ').data
+    
+    def test_bad_file_upload(self):
+        """tests failure to provide collection names or to choose a file"""
+        rv = self.upload_file('', 'hw_8_data/homework_8_refs.bib')
+        assert b'enter a valid collection name' in rv.data
+        data = {'collection_name': 'ex' }
+
+        rv = self.app.post('/upload_file/', buffered=True,
+                         content_type='multipart/form-data',
+                         data=data)
+        assert b'Please Choose a File' in rv.data
+    #def test_collection_size(self):
+    #    """make sure collection size works"""
+    
+    
 
 if __name__ == '__main__':
     unittest.main()
